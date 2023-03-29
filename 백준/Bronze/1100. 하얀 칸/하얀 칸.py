053b5070a978c855658b2a6041e61c5b2253fbe0
YGC20 <126743685+YGC20@users.noreply.github.com>
2023-03-30 01:24:01 +0900

    [Bronze II] Title: 하얀 칸, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.py
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -1,32 +1,17 @@
-def Chess(i,j,arr,count):
-    if (0 <= i < 8):
-        if i%2 == 0:
-            if(0 <= j < 8):
-                if(j == 0 or j == 2 or j == 4 or j == 6):
-                    if (arr[i][j] == 'F'): count += 1
-                    Chess(i,j+1,arr,count)
-                else: Chess(i,j+1,arr,count)
-            else:
-                j = 0
-                Chess(i+1,j,arr,count)
-        elif i%2 == 1:
-            if(0 <= j < 8):
-                if(j == 1 or j == 3 or j == 5 or j == 7):
-                    if (arr[i][j] == 'F'): count += 1
-                    Chess(i,j+1,arr,count)
-                else: Chess(i,j+1,arr,count)
-            else:
-                j = 0
-                Chess(i+1,j,arr,count)             
-    else :
-        print(count)
-
 arr = list()
+Chess = 0
 
-for i in range(8) :
+for i in range(8):
     arr.append(input())
 
-i = 0
-j = 0
-count = 0
-Chess(i,j,arr,count)
+for x in range(8):
+    if x%2 == 0:
+        for y in range(8):
+            if y%2 == 0:
+                if arr[x][y] == 'F': Chess += 1
+    elif x%2 == 1:
+        for y in range(8):
+            if y%2 == 1:
+                if arr[x][y] == 'F': Chess += 1
+
+print(Chess)
