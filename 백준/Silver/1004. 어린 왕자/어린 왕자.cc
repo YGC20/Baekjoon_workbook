f4f3f2d7a58ceba331bf3e4f32b128dc16967ea1
YGC20 <126743685+YGC20@users.noreply.github.com>
2025-01-02 04:30:55 +0900

    [Silver III] Title: 어린 왕자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.cc
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.cc
@@ -2,18 +2,17 @@
 using namespace std;
 
 int main(void){
-	int t, x1, y1, x2, y2, n;
-	int x, y, r, check1, check2, count;
+	int t;
 	scanf("%d", &t);
 	for(int i=0; i<t; i++){
+		int x1, y1, x2, y2, n, count;
 		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 		scanf("%d", &n);
 		count = 0;
 		for(int j=0; j<n; j++){
+			int x, y, r;
 			scanf("%d %d %d", &x, &y, &r);
-			check1 = (x1-x)*(x1-x)+(y1-y)*(y1-y) < r*r;
-			check2 = (x2-x)*(x2-x)+(y2-y)*(y2-y) < r*r;
-			if(check1 != check2) count++;
+			if(((x1-x)*(x1-x)+(y1-y)*(y1-y) < r*r) != ((x2-x)*(x2-x)+(y2-y)*(y2-y) < r*r)) count++;
 		}
 		printf("%d\n", count);
 	}
