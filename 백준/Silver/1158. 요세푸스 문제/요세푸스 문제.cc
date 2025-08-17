c46bef88fc01773cbbf2b2ff32cd5252cb430afd
YGC20 <126743685+YGC20@users.noreply.github.com>
2025-08-17 17:33:11 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int N,K;
+    cin>>N>>K;
+    queue<int> q;
+    for(int i=1;i<=N;i++)   q.push(i);
+    cout<<"<";
+    while(!q.empty()){
+        for(int i=0;i<K-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
+        cout<<q.front();
+        q.pop();
+        if(!q.empty())  cout<<", ";
+    }
+    cout<<">\n";
+    return 0;
+}
