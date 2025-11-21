f6f79a030212fcd0e05820848ad28ff92e26ba55
YGC20 <126743685+YGC20@users.noreply.github.com>
2025-11-21 17:07:34 +0900

    [D1] Title: 홀수만 더하기, Time: 5 ms, Memory: 372 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case) {
+		int sum = 0, num;
+        for(int i=0;i<10;++i){
+            cin>>num;
+            if(num%2!=0)
+                sum+=num;
+        }
+        cout<<'#'<<test_case<<" "<<sum<<"\n";
+	}
+	return 0;
+}
