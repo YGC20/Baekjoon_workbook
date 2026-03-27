fddb273bf6fdb346494a9095673ce8f8e357effe
YGC20 <126743685+YGC20@users.noreply.github.com>
2026-03-28 02:30:35 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int min(int a, int b)
+{
+    return a>b?b:a;
+}
+
+int solution(string name)
+{
+    int cnt = 0;
+    int len = name.length();
+    int move = len-1;
+    
+    for(int i=0; i<len; ++i) {
+        cnt += (name[i]-'A'<=13?name[i]-'A':26-(name[i]-'A'));
+        int cursor = i+1;
+        while(cursor<len && name[cursor]=='A') {
+            cursor++;
+        }
+        move = min(move, i+len-cursor+min(i,len-cursor));
+    }
+    return cnt+move;
+}
