d8cb473c77cab62173d5b72c0b91e09a94adbbd8
YGC20 <126743685+YGC20@users.noreply.github.com>
2026-04-22 23:37:49 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 0.28 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    for(int i=0; i<queries.size(); i++) {
+        int min = 1000001;
+        for(int j=queries[i][0]; j<=queries[i][1]; j++) {
+            if(arr[j]>queries[i][2] && min>arr[j])
+                min = arr[j];
+        }
+        if(min>=1000001) min = -1;
+        answer.push_back(min);
+    }
+    return answer;
+}
