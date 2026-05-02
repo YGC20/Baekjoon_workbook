a8b0f9e4b0b32a33e786aca1d4defec11f85ab7b
YGC20 <126743685+YGC20@users.noreply.github.com>
2026-05-02 20:49:16 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 330.33 ms, Memory: 223 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    for(auto& a : arr) {
+        if(a>=50 && a%2==0)
+            a/=2;
+        else if(a<50 && a%2!=0)
+            a*=2;
+    }
+    return arr;
+}
