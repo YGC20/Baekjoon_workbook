82e947c08aa317c804185d58f4b310e49145519a
YGC20 <126743685+YGC20@users.noreply.github.com>
2026-05-04 22:49:48 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.17 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+vector<string> solution(string my_string)
+{
+    vector<string> answer;
+    istringstream iss(my_string);
+    string tmp;
+    while(iss>>tmp) {
+        answer.push_back(tmp);
+    }
+    return answer;
+}
