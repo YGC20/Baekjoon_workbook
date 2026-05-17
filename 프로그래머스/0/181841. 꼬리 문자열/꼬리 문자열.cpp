2dc52bd644a985c62352ec27f90de3ffe19554cd
YGC20 <126743685+YGC20@users.noreply.github.com>
2026-05-17 17:23:48 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(vector<string> str_list, string ex)
+{
+    string answer = "";
+    for(auto sl : str_list) {
+        if(sl.find(ex)==string::npos) {
+            answer+=sl;
+        }
+    }
+    return answer;
+}
