dfbf402826ff0e4a7a60a9e74e8b6c071f4ef89c
YGC20 <ygchun123@gmail.com>
2026-06-04 09:02:50 +0900

    06/02 2572번 문제 풀이

--- 2572.c
+++ 2572.c
@@ -1,24 +1,46 @@
 ﻿/**
 * 정올
 * 회전초밥 (2572)
+* 슬라이딩 기법 이용
 */
 #if 01
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main(void)
 {
 	int N, d, k, c; (void)scanf("%d %d %d %d", &N, &d, &k, &c);
 
-	int* nums = (int*)malloc(sizeof(int) * N);
-	bool* visited = (bool*)malloc(sizeof(bool) * d);
+	int* nums = (int*)calloc(N * 2, sizeof(int));
+	int* visited = (int*)calloc(d+1, sizeof(int));
 	if (nums == NULL || visited == NULL) return -1;
 
 	for (int i = 0; i < N; ++i) {
 		(void)scanf("%d", &nums[i]);
+		nums[i + N] = nums[i];
 	}
 
+	int cnt = 0;
+	for (int i = 0; i < k; ++i) {
+		visited[nums[i]]++;
+		if (visited[nums[i]] == 1) cnt++;
+	}
+
+	int max = cnt;
+	for (int i = 1; i < (2 * N) - k; ++i) {
+		visited[nums[i - 1]]--;
+		if (visited[nums[i - 1]] == 0) cnt--;
+		
+		visited[nums[i + (k-1)]]++;
+		if (visited[nums[i + (k-1)]] == 1) cnt++;
+
+		if (max <= cnt) {
+			if (visited[c] == 0) cnt++;
+			max = cnt;
+		}
+	}
+	printf("%d\n", max);
 	return 0;
 }
 #endif
