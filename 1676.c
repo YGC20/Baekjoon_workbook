3bd719c52c9961d6593d6f0697ecef3709f0f557
YGC20 <ygchun123@gmail.com>
2026-06-09 19:15:33 +0900

    색종이 (고) 문제 풀이 작성중

--- 1676.c
+++ 1676.c
@@ -1,7 +1,11 @@
 ﻿/**
 * 1676 : 색종이 (고)
+3
+3 7
+15 7
+5 2
 */
-#if 0
+#if 01
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,12 +25,59 @@ int main(void)
 		}
 	}
 
+	int maxX = 0;
+	unsigned char lengthMapX[101][101] = { 0 };
+	for (int i = 0; i < 101; ++i) {
+		for (int j = 0; j < 101; ++j) {
+			if (whitePapper[i][j] >= 1) {
+				if (i == 0) lengthMapX[i][j] = 1;
+				else lengthMapX[i][j] = lengthMapX[i - 1][j] + 1;
+			}
+			else {
+				lengthMapX[i][j] = 0;
+			}
+			if (lengthMapX[i][j] > maxX) maxX = lengthMapX[i][j];
+		}
+	}
+	int cntY = 0;
+	for (int j = 0; j < 101; ++j) {
+		for (int i = 0; i < 101; ++i) {
+			if (lengthMapX[i][j] == maxX) cntY++;
+		}
+	}
+	int box1 = maxX * cntY;
+
+	int maxY = 0;
+	unsigned char lengthMapY[101][101] = { 0 };
+	for (int j = 0; j < 101; ++j) {
+		for (int i = 0; i < 101; ++i) {
+			if (whitePapper[i][j] >= 1) {
+				if (i == 0) lengthMapY[i][j] = 1;
+				else lengthMapY[i][j] = lengthMapY[i][j - 1] + 1;
+			}
+			else {
+				lengthMapY[i][j] = 0;
+			}
+			if (lengthMapY[i][j] > maxY) maxY = lengthMapY[i][j];
+		}
+	}
+	int cntX = 0;
+	for (int i = 0; i < 101; ++i) {
+		for (int j = 0; j < 101; ++j) {
+			if (lengthMapY[i][j] == maxY) cntX++;
+		}
+	}
+	int box2 = maxY * cntX;
+
+	printf("%d\n", (box1 > box2) ? box1 : box2);
+	return 0;
+}
+#endif
+
+/*
 	for (int x = 0; x < 101; ++x) {
 		for (int y = 0; y < 101; ++y) {
 			printf("%d", whitePapper[x][y]);
 		}printf("\n");
 	}printf("\n");
-
-	return 0;
-}
-#endif
+*/
